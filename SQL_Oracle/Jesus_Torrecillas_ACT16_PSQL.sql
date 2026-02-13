f307d693d94dee6f23c4f6f65af22b2e9c79842f
JesusTorrecillas4 <jtorrecillashidalgo@gmail.com>
2026-02-13 10:37:35 +0100

    Nueva actividad de PSQL

---
+++ SQL_Oracle/Jesus_Torrecillas_ACT16_PSQL.sql
@@ -0,0 +1,73 @@
+--ACT16 PSQL
+
+
+/*EXERCICI 1:  
+Defineix un bloc anònim amb les següents variables definides i mostra el seu valor amb < 
+DBMS_OUTPUT.PUT_LINE> 
+DECLARE 
+v_hiredate DATE; 
+v_deptno NUMBER(2) NOT NULL := 10; 
+v_location VARCHAR2(13) := 'Atlanta'; 
+c_comm CONSTANT NUMBER := 1400; */
+
+
+
+
+
+/*EXERCICI 2:  
+Digueu si aquests identificadors són vàlids per definir variables i per què: 
+v_A#$ 
+_num 
+X2 
+2X 
+v_num 
+#A */
+
+
+
+
+
+/*EXERCICI 3: 
+Digueu si aquestes declaracions són vàlides o no i per què: 
+v_id NUMBER(4); 
+v_x, v_y, v_z VARCHAR2(10); 
+v_birthdate DATE NOT NULL; 
+v_in_stock BOOLEAN:=1; 
+emp_ename emp_ename_type;*/
+
+
+
+
+/*
+EXERCICI 4: 
+Defineix un bloc anònim que mostri el nom i cognom del treballador 107. Mostra a la sortida del 
+programa el text: “El treballador és: <nom> <cognom>” */
+
+
+
+
+/*EXERCICI 5: 
+Defineix un bloc anònim que mostri el nom del departament o treballa l’empleat 109. Mostra a la 
+sortida del programa el text: “<nom> <cognom> treballa al departament <nom del 
+departament>” */
+
+
+
+
+/*EXERCICI 6: 
+Defineix un bloc anònim amb dues variables anomenades v_empleat1 i v_empleat2 a la que 
+signarem dos ids d’empleat (per exemple 107 i 109). El programa buscarà la localització de cada 
+empleat i mostrarà dos missatges informant del carrer, ciutat, codi postal i província dels 
+dos treballadors. */
+
+
+
+
+/*EXERCICI 7: 
+Defineix un bloc anònim que mostri missatges amb els totals de registres de les taules 
+EMPLOYEES, DEPARTMENTS, LOCATIONS i COUNTRIES. */
+
+
+
+
+
