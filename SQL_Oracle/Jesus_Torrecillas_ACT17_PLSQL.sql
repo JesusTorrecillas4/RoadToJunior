4675aaa31d7c4ba0b39a53ec10510489dd6f3312
JesusTorrecillas4 <jtorrecillashidalgo@gmail.com>
2026-02-18 12:50:20 +0100

    PLSQL y HTML

---
+++ SQL_Oracle/Jesus_Torrecillas_ACT17_PLSQL.sql
@@ -0,0 +1,29 @@
+-- ACT 17 PLSQL
+
+
+
+/*
+EXERCICI 1: 
+Defineix un bloc anònim amb dues variables anomenades v_dept_id i v_job_id a la que 
+assignarem l’id d’un departament i el d’un job (per exemple 80 i SA_REP). El programa buscarà 
+el nombre del departament i el nom del job i els mostrarà per sortida de consola.
+*/
+
+
+/*
+EXERCICI 2: 
+Defineix un procediment al qual li passem l'id d'un empleat i mostrem un missatge amb el seu 
+nom, cognom i el nombre de treballadors que té a càrrec seu (de quanta gent és mànager).  
+Per exemple: Si assignem l'ID 100, retorna el missatge "Steven King és mànager de 14 
+empleats". 
+Si el treballador no existeix, ha de retornar un missatge informant de l’error. 
+*/
+
+
+/*
+EXERCICI3: 
+Defineix un procediment al que li passarem una variable d’id de departament. El codi buscarà 
+quant es gasta el departament en salaris de treballadors i retornarà un missatge en funció del 
+valor. - Si la suma és superior a 19000, es retornarà el missatge "Es gasten molt!". - Si la suma és inferior a 19000, es retornarà el missatge "Es gasten poc!". - Si la suma és exactament 19000, es retornarà el missatge "Es gasten justament 19000!". 
+Prova amb els valors de department (10, 20 i 80).
+*/
