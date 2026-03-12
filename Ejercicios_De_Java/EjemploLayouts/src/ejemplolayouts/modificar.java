f7970dde609089498061d0c693645081f91bf906
JesusTorrecillas4 <jtorrecillashidalgo@gmail.com>
2026-03-12 20:56:58 +0100

    Mejora del CSS en portfolio y retoque de layouts

---
+++ Ejercicios_De_Java/EjemploLayouts/src/ejemplolayouts/modificar.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplolayouts;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author jesus
+ */
+public class modificar extends JFrame {
+    
+    
+    public modificar(){
+        
+        setTitle("Editor");
+        setSize(400,400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        
+        
+    }
+    
+   
+    
+    
+    
+    
+    
+    
+    
+}
