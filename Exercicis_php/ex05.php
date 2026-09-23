24aa2d32a2b1efe7377f7b2106037470048099ce
Jesus Torrecillas <jtorrecillashidalgo@gmail.com>
2026-09-23 12:22:35 +0200

    nuevos ejercicios de php

---
+++ Exercicis_php/ex05.php
@@ -0,0 +1,18 @@
+<?php
+
+$nom = 'Aina';
+$n = 3;
+
+//prediula sortida de:
+
+echo 'Hola $nom'; --> Hola $nom
+echo "<br>"
+echo 'Hola $nom'; --> Hola $nom
+echo "<br>"
+echo 'Hola $nom'; --> Hola $nom
+echo "<br>"
+echo 'Hola $nom'; --> Hola $nom
+echo "<br>"
+
+
+?>
